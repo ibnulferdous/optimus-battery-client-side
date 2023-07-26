42227929c32a5d54efb6246275757fdc51c73c53
Ibnul Ferdous <ibnul04@gmail.com>
2023-07-26 21:54:32 +0600

    Componenet name fixed

--- pages/products/[path].js
+++ pages/products/[path].js
@@ -1,20 +1,7 @@
 // Mongodb connection
 import clientPromise from "../../lib/mongodb";
-
-import { Box, Container, Paper, Typography } from "@mui/material";
-import Grid from "@mui/material/Unstable_Grid2";
-import List from "@mui/material/List";
-import ListItem from "@mui/material/ListItem";
-import ListItemText from "@mui/material/ListItemText";
-import ListItemAvatar from "@mui/material/ListItemAvatar";
-import Avatar from "@mui/material/Avatar";
-import Image from "next/image";
 import Head from "next/head";
-
-// Material Icon
-import SafetyCheckIcon from "@mui/icons-material/SafetyCheck";
-import BatteryChargingFullIcon from "@mui/icons-material/BatteryChargingFull";
-import ProductIntro from "../../components/singleProductPage/productIntro";
+import ProductIntro from "../../components/singleProductPage/ProductIntro";
 
 export default function SingleProduct({ product }) {
   const {
@@ -38,107 +25,18 @@ export default function SingleProduct({ product }) {
     <>
       {/* Head part for meta data */}
       <Head>
-        <title>{model}Ah- IPS/UPS/Inverter battery</title>
+        <title>{`${model}Ah- IPS/UPS/Inverter battery`}</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main>
-        <ProductIntro />
-        <Container sx={{ marginBlock: "100px" }} fixed>
-          {/* First Section starts */}
-          <Box>
-            <Grid container spacing={12}>
-              <Grid xs={12} md={6}>
-                <Paper variant="outlined" square>
-                  <Image
-                    src="/images/100-dry-cell-battery.jpg"
-                    width={600}
-                    height={600}
-                    alt="Optimus 100Ah dry cell battery for ips/ups/inverter"
-                    style={{
-                      maxWidth: "100%",
-                      height: "auto",
-                    }}
-                  />
-                </Paper>
-              </Grid>
-              <Grid xs={12} md={6}>
-                <Typography
-                  variant="h4"
-                  component="h1"
-                  sx={{ fontWeight: "700", marginBottom: "25px" }}
-                >
-                  {model}Ah
-                </Typography>
-
-                <Typography
-                  variant="h6"
-                  component="h3"
-                  sx={{
-                    marginBottom: "35px",
-                    fontWeight: "700",
-                    color: "#2962ff",
-                  }}
-                >
-                  Price: ৳ {price}
-                </Typography>
-
-                <Typography
-                  variant="body1"
-                  gutterBottom
-                  sx={{ lineHeight: "1.8" }}
-                >
-                  {description}
-                </Typography>
-
-                <Box>
-                  <Grid container spacing={2}>
-                    <Grid xs={6}>
-                      <List
-                        sx={{
-                          width: "100%",
-                        }}
-                      >
-                        <ListItem>
-                          <ListItemAvatar>
-                            <Avatar sx={{ bgcolor: "#2962ff" }}>
-                              <BatteryChargingFullIcon />
-                            </Avatar>
-                          </ListItemAvatar>
-                          <ListItemText
-                            primary="Capacity"
-                            secondary={`${capacity}Ah`}
-                          />
-                        </ListItem>
-                      </List>
-                    </Grid>
-
-                    <Grid xs={6}>
-                      <List
-                        sx={{
-                          width: "100%",
-                        }}
-                      >
-                        <ListItem>
-                          <ListItemAvatar>
-                            <Avatar sx={{ bgcolor: "#2962ff" }}>
-                              <SafetyCheckIcon />
-                            </Avatar>
-                          </ListItemAvatar>
-                          <ListItemText
-                            primary="Warrenty"
-                            secondary={`${warrenty}* Months`}
-                          />
-                        </ListItem>
-                      </List>
-                    </Grid>
-                  </Grid>
-                </Box>
-              </Grid>
-            </Grid>
-          </Box>
-          {/* First Section ends */}
-        </Container>
+        <ProductIntro
+          model={model}
+          capacity={capacity}
+          description={description}
+          price={price}
+          warrenty={warrenty}
+        />
       </main>
     </>
   );
