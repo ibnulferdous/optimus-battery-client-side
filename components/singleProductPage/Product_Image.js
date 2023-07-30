4601f7d3e7fe74c6a1b303da3d4319c6a0f6c5d9
Ibnul Ferdous <ibnul04@gmail.com>
2023-07-30 16:36:46 +0600

    Product page image button added

--- components/singleProductPage/Product_Image.js
+++ components/singleProductPage/Product_Image.js
@@ -1,25 +1,57 @@
-import ImageGallery from "react-image-gallery";
+import Box from "@mui/material/Box";
+import IconButton from "@mui/material/IconButton";
+import Image from "next/image";
+import Paper from "@mui/material/Paper";
+import Stack from "@mui/material/Stack";
+import { useState } from "react";
 
-const images = [
-  {
-    original: "https://picsum.photos/id/1018/1000/600/",
-    thumbnail: "https://picsum.photos/id/1018/250/150/",
-  },
-  {
-    original: "https://picsum.photos/id/1015/1000/600/",
-    thumbnail: "https://picsum.photos/id/1015/250/150/",
-  },
-  {
-    original: "https://picsum.photos/id/1019/1000/600/",
-    thumbnail: "https://picsum.photos/id/1019/250/150/",
-  },
-];
+const ProductImage = ({ images, model }) => {
+  const [imageUrl, setImageUrl] = useState(images[0]);
 
-const ProductImage = () => {
   return (
-    <div>
-      <ImageGallery items={images} />
-    </div>
+    <>
+      <Paper variant="outlined" square>
+        <Image
+          src={`/images/${imageUrl}`}
+          alt={`${model} battery for ips/ups/inverter`}
+          placeholder="empty"
+          width={550}
+          height={550}
+          priority="true"
+          quality={100}
+          style={{
+            maxWidth: "100%",
+            height: "auto",
+          }}
+        />
+      </Paper>
+      <Box sx={{ display: "flex", justifyContent: "center", pt: 2 }}>
+        <Stack direction="row" spacing={2}>
+          {images.map((image) => (
+            <Paper variant="outlined" square key={image}>
+              <IconButton
+                aria-label="Image button"
+                sx={{ borderRadius: 0 }}
+                onClick={() => setImageUrl(image)}
+              >
+                <Image
+                  src={`/images/${image}`}
+                  alt={`${model} battery for ips/ups/inverter`}
+                  placeholder="empty"
+                  width={50}
+                  height={50}
+                  priority="true"
+                  style={{
+                    maxWidth: "100%",
+                    height: "auto",
+                  }}
+                />
+              </IconButton>
+            </Paper>
+          ))}
+        </Stack>
+      </Box>
+    </>
   );
 };
 
