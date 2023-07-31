e508aabcdf97d061be5545adafa00093e733e193
tazrian-amin <amin.tazrian1979@gmail.com>
2023-07-31 17:27:23 +0600

    added dialogue to update price

--- components/ProductsTable.js
+++ components/ProductsTable.js
@@ -6,16 +6,16 @@ import ActionsFieldRenderer from "./ActionsFieldRenderer";
 
 const ProductsTable = () => {
   const products = [
-    { index: 1, model: "Optimus 100AH", _id: 11 },
-    { index: 2, model: "Optimus 130AH", _id: 22 },
-    { index: 3, model: "Optimus 165AH", _id: 33 },
+    { index: 1, model: "Optimus 100AH", _id: 11, price: 100 },
+    { index: 2, model: "Optimus 130AH", _id: 22, price: 200 },
+    { index: 3, model: "Optimus 165AH", _id: 33, price: 300 },
   ];
 
   const gridApiRef = useRef(null);
 
   const actionsCellRenderer = (props) => {
-    const { _id } = props.data;
-    return <ActionsFieldRenderer id={_id} />;
+    const { _id, model, price } = props.data;
+    return <ActionsFieldRenderer id={_id} model={model} price={price} />;
   };
 
   const columnDefs = [
@@ -27,6 +27,12 @@ const ProductsTable = () => {
     {
       headerName: "Model Name",
       field: "model",
+      filter: true,
+      flex: 1,
+    },
+    {
+      headerName: "Price",
+      field: "price",
       filter: true,
       flex: 1,
     },
